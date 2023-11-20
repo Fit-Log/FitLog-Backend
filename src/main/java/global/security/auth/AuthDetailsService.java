ccbe2af54082d202a9d62fd7ae6d590639807595
김가은 <kge02160@gmail.com>
2023-11-20 17:30:29 +0900

    ⚡️ :: AuthDetailsService 추가

---
+++ src/main/java/global/security/auth/AuthDetailsService.java
@@ -0,0 +1,25 @@
+package global.security.auth;
+
+import com.example.fitlog.domain.user.domain.repository.UserRepository;
+import com.example.fitlog.domain.user.exception.UserNotFoundException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+@Service
+public class AuthDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String accountId) throws UsernameNotFoundException {
+        return userRepository.findByAccountId(accountId)
+                .map(AuthDetails::new)
+                .orElseThrow(() -> UserNotFoundException.EXCEPTION);
+    }
+}
