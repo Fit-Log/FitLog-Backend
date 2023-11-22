df57e69ae2b0cb29e9512d65f5c25f5cc96b2b84
김가은 <kge02160@gmail.com>
2023-11-22 22:17:53 +0900

    ⚡️ :: LogoutService 추가

---
+++ src/main/java/com/example/fitlog/domain/auth/service/LogoutService.java
@@ -0,0 +1,27 @@
+package com.example.fitlog.domain.auth.service;
+
+import com.example.fitlog.domain.auth.domain.RefreshToken;
+import com.example.fitlog.domain.auth.domain.repository.RefreshTokenRepository;
+import com.example.fitlog.domain.user.domain.User;
+import com.example.fitlog.domain.user.exception.RefreshTokenNotFoundException;
+import com.example.fitlog.domain.user.facade.UserFacade;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class LogoutService {
+
+    private final RefreshTokenRepository refreshTokenRepository;
+    private final UserFacade userFacade;
+
+    @Transactional
+    public void execute() {
+        User user = userFacade.getCurrentUser();
+
+        RefreshToken refreshToken = refreshTokenRepository.findById(user.getAccountId())
+                .orElseThrow(() -> RefreshTokenNotFoundException.EXCEPTION);
+        refreshTokenRepository.delete(refreshToken);
+    }
+}
