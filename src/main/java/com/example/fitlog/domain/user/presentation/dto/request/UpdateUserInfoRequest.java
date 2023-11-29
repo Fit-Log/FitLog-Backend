5d68e894a3597da0f10e9502a04e8db10ad5bb87
김가은 <kge02160@gmail.com>
2023-11-29 16:10:23 +0900

    ⚡️ :: UpdateUserInfoRequest 코드 추가

--- src/main/java/com/example/fitlog/domain/user/presentation/dto/request/UpdateUserInfoRequest.java
+++ src/main/java/com/example/fitlog/domain/user/presentation/dto/request/UpdateUserInfoRequest.java
@@ -1,10 +1,15 @@
 package com.example.fitlog.domain.user.presentation.dto.request;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
 @NoArgsConstructor
 public class UpdateUserInfoRequest {
-    
+
+    @NotBlank(message = "공백은 불가합니다.")
+    private Double weight;
+
+    private Double height;
 }
